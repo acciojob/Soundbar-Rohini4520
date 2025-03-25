26e12b2d2b873dc255108d6a216a5b2985a0d15b
Rohini4520 <146929586+Rohini4520@users.noreply.github.com>
2025-03-25 15:04:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 //your JS code here. If required.
-const sound=['applause' , 'boo', 'gasp','tada','victory','wrong','stop'];
- function playSound(sound) {
-      stopSounds();
-      const audio = new Audio(`./sounds/${sound}.mp3`);
-      audio.play();
-      audio.id = sound;
-      document.body.appendChild(audio);
-    }
+<script>
+  window.sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
 
-    function stopSounds() {
-      sounds.forEach(sound => {
-        const audio = document.getElementById(sound);
-        if (audio) {
-          audio.pause();
-          audio.currentTime = 0;
-          audio.remove();
-        }
-      });
-    }
-window.playSound = playSound;
-    window.stopSounds = stopSounds;
+  function playSound(sound) {
+    stopSounds();
+    const audio = new Audio(`./sounds/${sound}.mp3`);
+    audio.play();
+    audio.id = sound;
+    document.body.appendChild(audio);
+  }
 
+  function stopSounds() {
+    window.sounds.forEach(sound => {
+      const audio = document.getElementById(sound);
+      if (audio) {
+        audio.pause();
+        audio.currentTime = 0;
+        audio.remove();
+      }
+    });
+  }
 
+  window.playSound = playSound;
+  window.stopSounds = stopSounds;
